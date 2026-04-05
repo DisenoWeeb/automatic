37cccd840a34f400ac1d67882c888ce358d0eb42
Ronaldo <weebargentina@gmail.com>
2026-04-05 01:40:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -178,23 +178,7 @@ const App = {
       e.btnCompartir.addEventListener('click', () => this.compartirImagen());
     }
   },
- // 👇 ACA VA
-  dataURLtoExactFile(dataUrl, filename) {
-    const arr = dataUrl.split(',');
-    const mimeMatch = arr[0].match(/:(.*?);/);
-    const mime = mimeMatch ? mimeMatch[1] : 'image/png';
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    return Promise.resolve(new File([u8arr], filename, { type: mime }));
-  },
-
-};
+ 
   bindCanvasInteractions() {
     if (!this.canvas) return;
 
